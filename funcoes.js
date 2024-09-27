7d30c0b9d8885ad91f040e0f14baa026a975be29
Débora Gonçalves <s.goncalves.debora@gmail.com>
2024-09-27 15:42:20 -0300

    exercicio typescripts

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicacao(n1, n2) {
+    return n1 * n2;
+}
+function dizOla(nome) {
+    return `Olá ${nome}`;
+}
+const resultado = multiplicacao(10, 10);
+console.log(resultado);
+const saudacao = dizOla("tutores");
+console.log(saudacao);
---
+++ funcoes.ts
@@ -0,0 +1,15 @@
+function multiplicacao (n1: number, n2: number): number {
+    return n1 * n2;
+}
+
+
+function dizOla (nome: string): string {
+    return `Olá ${nome}`;
+}
+
+
+const resultado = multiplicacao(10, 10);
+console.log(resultado);
+
+const saudacao = dizOla ("tutores");
+console.log(saudacao);
